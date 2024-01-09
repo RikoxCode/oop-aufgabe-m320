915ccc6af83c70373dd0d6fd032975360c4ef0b3
Netshlife <levinzimmermann06@gmail.com>
2024-01-09 16:27:49 +0100

    added Driveable cars

---
+++ programm/AnimalFarm/AnimalFarm/IDriveable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFarm
+{
+    internal interface IDriveable
+    {
+        string LicenesePLate { get; set; }
+
+        void setOwner(Farmer farmer);
+
+        void getOwner(Farmer owner);
+    }
+}
